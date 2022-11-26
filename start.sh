05e6f5fe215a5d5a599e12ea2907faea489a7547
Godstime315 <97334195+Godstime315@users.noreply.github.com>
2022-11-26 10:48:26 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Hislordshippius/zendaya /zendaya
+  git clone https://github.com/JstathamTG/Vindiesel /Vindiesel
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /zendaya
+  git clone $UPSTREAM_REPO /Vindiesel
 fi
-cd /zendaya
+cd /Vindiesel
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
